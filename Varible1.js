1e4e730112f6fd4c78a1cc94a0fc6a6c3598488b
Alex-Messi <33alexmuha2007@gmail.com>
2023-07-06 14:47:51 +0300

    Variable2

--- Varible1.js
+++ Varible1.js
@@ -1,3 +1,31 @@
-let user = 'John';
+/*let user = 'John';
 let age = 25;
 let message = 'Hello';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+alert(color)
+let admin, name;
+
+name = "Джон";
+
+admin = name;
+
+alert( admin );
+*/
+let water = 'Земля';
+
+let username = 'Джон';
+
+
+
+
+
+
+
+
+
